9371141631422935fa25da98693429d49734c074
Divya <divyasarathi2296@gmail.com>
2023-07-12 00:09:32 +0530

    first commit

---
+++ Prime.js
@@ -0,0 +1,20 @@
+function sumPrimes(num) {
+    let numArray = [];
+    for (let i = 1; i <= num; i++) {
+      numArray.push(i);
+    }
+       
+    numArray.map((number) => {
+      for (let i = 2; i < number; i++) {
+          if(number % i === 0) {
+              let index = numArray.indexOf(number);
+              return numArray.splice(index, 1);       
+          }
+      }   
+    });
+  
+   return numArray;
+  
+  }
+  
+  sumPrimes(10);
---
+++ sum.js
@@ -0,0 +1,8 @@
+let a=[1,3,9,6,3,5]
+let sum=0
+for(let i=0;i<a.length;i++)
+{
+    sum = sum+a[i];
+
+}
+console.log(sum)
