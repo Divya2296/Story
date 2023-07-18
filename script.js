b8ce1e545410bbd6114fc187bfee5b700a3db9c6
Divya2296 <133556646+Divya2296@users.noreply.github.com>
2023-07-18 23:49:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+var countdown = document.querySelector("#countdown") 
+var c = 10;
+setTimeout( ()=> { 
+    console.log("cd", c) 
+    countdown.innerText = c ;
+    c--;  
+    setTimeout( ()=> { 
+        console.log("cd", c) 
+        countdown.innerText = c ;
+        c--;  
+        setTimeout( ()=> { 
+            console.log("cd", c) 
+            countdown.innerText = c ;
+            c--;  
+            setTimeout( ()=> { 
+                console.log("cd", c) 
+                countdown.innerText = c ;
+                c--;  
+                setTimeout( ()=> { 
+                    console.log("cd", c) 
+                    countdown.innerText = c ;
+                    c--;  
+                    setTimeout( ()=> { 
+                        console.log("cd", c) 
+                        countdown.innerText = c ;
+                        c--;  
+                        setTimeout( ()=> { 
+                            console.log("cd", c) 
+                            countdown.innerText = c ;
+                            c--;  
+                            setTimeout( ()=> { 
+                                console.log("cd", c) 
+                                countdown.innerText = c ;
+                                c--;  
+                                setTimeout( ()=> { 
+                                    console.log("cd", c) 
+                                    countdown.innerText = c ;
+                                    c--;  
+                                    setTimeout( ()=> { 
+                                        console.log("cd", c) 
+                                        countdown.innerText = c ;
+                                        c--;  
+                                        setTimeout( ()=> { 
+                                            console.log("indep")
+                                            countdown.innerText = "HAPPY INDEPENDENCE DAY";  
+                                            countdown.style.color = "red";
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
